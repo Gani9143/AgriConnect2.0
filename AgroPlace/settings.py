f54f1a2ce6eb68934e00d98329be4818e421f904
gani9143 <cganeshgani13@gmail.com>
2025-11-28 13:21:19 +0530

    added scikit-learn

--- AgroPlace/settings.py
+++ AgroPlace/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret-key-change-me")
 
 DEBUG = os.environ.get("DEBUG", "True") == "True"
 
-ALLOWED_HOSTS = ["*"]   # later we can set your Render domain here
+ALLOWED_HOSTS = [".onrender.com", "localhost", "127.0.0.1"]  # Compatible with Render deployment
 
 
 
@@ -130,8 +130,8 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_URL = '/meida/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'meida')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 from django.contrib.messages import constants as messages
